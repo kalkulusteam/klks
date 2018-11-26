f7f964b34b12dc4e082410f89e84f9cc0fde6ff9
kalkulusteam <32717997+kalkulusteam@users.noreply.github.com>
2018-11-26 12:58:45 +0100

    Fix splash copyright

--- src/clientversion.h
+++ src/clientversion.h
@@ -39,7 +39,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Dash Core Developers, 2015-" STRINGIZE(COPYRIGHT_YEAR) " The PIVX Client Developers, 2018" STRINGIZE(COPYRIGHT_YEAR) " The Kalkulus Client Developers, 2018"
+#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Dash Core Developers, 2015-" STRINGIZE(COPYRIGHT_YEAR) " The PIVX Core Developers, 2018-" STRINGIZE(COPYRIGHT_YEAR) " The Kalkulus Client Developers, 2018"
 
 /**
  * klksd-res.rc includes this file, but it cannot cope with real c++ code.
